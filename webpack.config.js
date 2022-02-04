0cfbf7f3d209a05b14216104585688218af16091
Will Greenberg <greenberg.will@gmail.com>
2022-02-04 15:58:49 -0800

    WIP having trouble with adding image using webpack

--- webpack.config.js
+++ webpack.config.js
@@ -35,18 +35,6 @@ module.exports = {
         loader: "eslint-loader"
       },
       {
-        test: /\.(gif|png|jpe?g)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'assets/images/'
-            }
-          }
-        ]
-      },
-      {
         test:/\.html$/,
         use: [
           'html-loader'
