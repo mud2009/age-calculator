682191187bc44eea14458f566604afecdfc67aa8
Will Greenberg <greenberg.will@gmail.com>
2022-02-04 15:03:21 -0800

    pass tests for venus life calculator

--- src/js/age.js
+++ src/js/age.js
@@ -5,10 +5,7 @@ export default class Age {
     this.venAge = 0;
     this.marsAge = 0;
     this.jupAge = 0;
-    this.satAge = 0;
-    this.urAge = 0;
-    this.nepAge = 0;
-    this.plutoAge = 0;
+
     this.life = 72.74; // this is the average life expectancy for humans
   }
 
@@ -17,10 +14,6 @@ export default class Age {
     this.venAge = Math.round((this.earthAge/.62)*10)/10;
     this.marsAge = Math.round((this.earthAge/1.88)*10)/10;
     this.jupAge = Math.round((this.earthAge/11.86)*10)/10;
-    this.satAge = Math.round((this.earthAge/29.46)*100)/100
-    this.urAge = Math.round((this.earthAge/84.01)*100)/100
-    this.nepAge = Math.round((this.earthAge/164.79)*100)/100
-    this.plutoAge = Math.round((this.earthAge/248.59)*100)/100
   }
 
   calcMercLife() {
@@ -28,12 +21,18 @@ export default class Age {
     if (this.mercLifeLeft < 0) {
       this.mercLifeLived = -this.mercLifeLeft;
     }
+  }
 
+  calcVenLife() {
+    this.venLifeLeft = Math.round((72.74 - this.venAge)*100)/100;
+    if (this.venLifeLeft < 0) {
+      this.venLifeLived = -this.venLifeLeft;
+    }
   }
 }
 
-// let age = new Age(4);
-// age.calcAge();
-// age.calcLife();  
-// console.log(age)
-// console.log(age.mercLifeLived)
+let age = new Age(100);
+age.calcAge();
+age.calcVenLife();  
+console.log(age)
+console.log(age.venLifeLived)
